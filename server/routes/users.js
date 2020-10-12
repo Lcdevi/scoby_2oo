8912606738d3c9b232366f1c74e88085dd0cf1bf
Lcdevi <caillauxlaura@gmail.com>
2020-10-12 16:29:12 +0200

    create cloudinary.js and create router.patch for the user

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/User");
+const uploader = require("../config/cloudinary");
+
 
 router.get("/", function (req, res, next) {
   res.send("respond with a resource");
 });
+
+router.patch("/me", uploader.single("image"), (req, res, next) => {
+  const updateValues = req.body;
+
+  if(req.file) {
+      updateValues.image = req.file.path;
+  }
+
+  User.findByIdAndUpdate(req.params.id, updateValues, {new: true} )
+      .then(userDocument => {
+          res.status(200).json(userDocument);
+      })
+      .catch(error => {
+          res.status(200).json(error);
+  })
+})
 
 module.exports = router;
